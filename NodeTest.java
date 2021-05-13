ab524b6df80c9c7b86b0f96d21284bd198656390
Alen Sugimoto <alensugimoto@gmail.com>
2021-05-13 13:53:47 +0200

    Make NodeTest.java compile

--- NodeTest.java
+++ NodeTest.java
@@ -2,7 +2,9 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import java.util.List;
+import java.util.LinkedList;
 
 
 /**
@@ -13,21 +15,25 @@ public class NodeTest {
     @Test
     public void testVariable() {
         Node e = new Variable(0, 0, 1);
-        List<String> context;
-        context.add(0, "x");
-        assertEquals("x", e.toString());
+        List<String> context = new LinkedList<>();
+        context.add("x");
+        assertEquals("x", e.toString(context));
     }
     
     @Test
     public void testAbstraction() {
-        Node e = new Abstraction(new Variable("x"), new Variable("x"));
-        assertEquals("(\\x.y)", e.toString());
+        Node e = new Abstraction(0, "x", new Variable(1, 0, 1));
+        List<String> context = new LinkedList<>();
+        assertEquals("(lambda x. x)", e.toString(context));
     }
     
     @Test
     public void testApplication() {
-        Node e = new Application(new Variable("x"), new Variable("y"));
-        assertEquals("(x y)", e.toString());
+        Node e = new Application(0, new Variable(1, 0, 2), new Variable(2, 1, 2));
+        List<String> context = new LinkedList<>();
+        context.add("x");
+        context.add("y");
+        assertEquals("(x y)", e.toString(context));
     }
     
 }
