061e4b07e166d9d52ffce72ebcd2b0c06677edec
Alen Sugimoto <alensugimoto@gmail.com>
2021-05-13 14:56:51 +0200

    Make test nodes realistic in NodeTest

--- NodeTest.java
+++ NodeTest.java
@@ -22,14 +22,14 @@ public class NodeTest {
     
     @Test
     public void testAbstraction() {
-        Node e = new Abstraction(0, "x", new Variable(1, 0, 1));
+        Node e = new Abstraction(0, "x", new Variable(3, 0, 1));
         List<String> context = new LinkedList<>();
         assertEquals("(\\x.x)", e.toString(context));
     }
     
     @Test
     public void testApplication() {
-        Node e = new Application(0, new Variable(1, 0, 2), new Variable(2, 1, 2));
+        Node e = new Application(0, new Variable(0, 0, 2), new Variable(2, 1, 2));
         List<String> context = new LinkedList<>();
         context.add("x");
         context.add("y");
