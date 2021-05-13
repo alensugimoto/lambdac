ca23745330c41c6f6b1538b7c9f0ee192a49c673
Alen Sugimoto <alensugimoto@gmail.com>
2021-05-13 14:43:00 +0200

    Make ParserTest.java compile

--- ParserTest.java
+++ ParserTest.java
@@ -26,9 +26,9 @@ public class ParserTest {
         // code under test
         final Node actualRoot = parser.parse(sourceCode);
         // expected tree
-        final Node expectedRoot = new Variable("x");
+        final Node expectedRoot = new Variable(0, 0, 1);
         // assertion
-        assertEquals(expectedRoot.toString(), actualRoot.toString());
+        assertEquals(expectedRoot, actualRoot);
     }
 
     @Test
@@ -36,13 +36,13 @@ public class ParserTest {
         // setup
         final Parser parser = new Parser();
         // test input
-        final String sourceCode = "\\x.y";
+        final String sourceCode = "\\x.x";
         // code under test
         final Node actualRoot = parser.parse(sourceCode);
         // expected tree
-        final Node expectedRoot = new Abstraction(new Variable("x"), new Variable("y"));
+        final Node expectedRoot = new Abstraction(0, "x", new Variable(3, 0, 1));
         // assertion
-        assertEquals(expectedRoot.toString(), actualRoot.toString());
+        assertEquals(expectedRoot, actualRoot);
     }
 
     @Test
@@ -54,9 +54,9 @@ public class ParserTest {
         // code under test
         final Node actualRoot = parser.parse(sourceCode);
         // expected tree
-        final Node expectedRoot = new Application(new Variable("x"), new Variable("y"));
+        final Node expectedRoot = new Application(0, new Variable(0, 0, 1), new Variable(2, 1, 2));
         // assertion
-        assertEquals(expectedRoot.toString(), actualRoot.toString());
+        assertEquals(expectedRoot, actualRoot);
     }
 
     @Test
@@ -68,9 +68,9 @@ public class ParserTest {
         // code under test
         final Node actualRoot = parser.parse(sourceCode);
         // expected tree
-        final Node expectedRoot = new Variable("x");
+        final Node expectedRoot = new Variable(1, 0, 1);
         // assertion
-        assertEquals(expectedRoot.toString(), actualRoot.toString());
+        assertEquals(expectedRoot, actualRoot);
     }
     
 }
