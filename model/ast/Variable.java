32c96ad19542aafd93306a75f5694cf8535df6e4
Alen Sugimoto <alensugimoto@gmail.com>
2021-05-14 10:07:04 +0200

    Restructure project

---
+++ model/ast/Variable.java
@@ -0,0 +1,51 @@
+package model.ast;
+
+import model.context.Context;
+
+/**
+ * A variable.
+ */
+public class Variable extends Node {
+    
+    private final int index;
+    private final int contextLength;
+
+    /**
+     * Create a new Variable node.
+     * @param name the name of this node
+     */
+    public Variable(final int position, final int index, final int contextLength) {
+        super(position);
+        this.index = index;
+        this.contextLength = contextLength;
+    }
+    
+    @Override
+    public Node termShift(final int c, final int d) {
+        return new Variable(
+            getPosition(),
+            index < c ? index : index + d,
+            contextLength + d);
+    }
+    
+    @Override
+    public Node termSubst(final int j, final int c, final Node s) {
+        return index == j + c ? s.termShift(c) : this;
+    }
+
+    @Override
+    public String toString(final Context context) {
+        return context.size() == contextLength ? context.get(index) : "[bad index]";
+    }
+    
+    @Override
+    public boolean equals(final Object obj) {
+        if (super.equals(obj) && obj instanceof Variable) {
+            Variable other = (Variable) obj;
+            return index == other.index && contextLength == other.contextLength;
+        } else {
+            return false;
+        }
+    }
+    
+}
---
+++ model/context/Context.java
@@ -0,0 +1,18 @@
+package model.context;
+
+import java.util.LinkedList;
+
+/**
+ * A Context contains information about the current naming context.
+ */
+public class Context extends LinkedList<String> {
+    
+    public Context() {
+        super();
+    }
+    
+    public Context(Context context) {
+        super(context);
+    }
+    
+}
