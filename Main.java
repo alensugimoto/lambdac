995d85f7ac4b8eaaadec12fab81c142879d32121
ArtMorgan0 <artmorgan101@gmail.com>
2021-05-16 00:06:51 +0200

    added first version of GUI

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
 import model.Interpreter;
+import gui.LambdaFrame;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
 
 public class Main {
     
     public static void main(final String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LambdaFrame().setVisible(true);
+            }
+        });
+        /**
         Scanner input = new Scanner(System.in);
         boolean f = true;
         while (f) {
@@ -14,6 +23,7 @@ public class Main {
                 System.out.println(new Interpreter().interpret(a));
             }
         }
+        */
     }
     
 }
