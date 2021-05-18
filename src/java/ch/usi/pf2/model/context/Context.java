514ff581e4b7257c74151e0434bae840b105c3e3
Alen Sugimoto <alensugimoto@gmail.com>
2021-05-18 21:33:17 +0200

    Add javadoc to constructors in Context

--- src/java/ch/usi/pf2/model/context/Context.java
+++ src/java/ch/usi/pf2/model/context/Context.java
@@ -1,18 +1,30 @@
 package ch.usi.pf2.model.context;
 
+import java.util.Collection;
 import java.util.LinkedList;
+
 
 /**
  * A Context contains information about the current naming context.
  */
 public class Context extends LinkedList<String> {
     
+    /**
+     * Constructs an empty context.
+     */
     public Context() {
         super();
     }
     
-    public Context(Context context) {
-        super(context);
+    /**
+     * Constructs a list containing the elements of the specified collection,
+     * in the order they are returned by the collection's iterator.
+     * 
+     * @param c the collection whose elements are to be placed into this context
+     * @see LinkedList#LinkedList(Collection)
+     */
+    public Context(Collection<? extends String> c) {
+        super(c);
     }
     
 }
