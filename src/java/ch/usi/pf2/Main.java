c7a86f2e05c5550e14617aa5768fe26a38780263
Alen Sugimoto <alensugimoto@gmail.com>
2021-05-19 23:37:10 +0200

    Add one command line argument

--- src/java/ch/usi/pf2/Main.java
+++ src/java/ch/usi/pf2/Main.java
@@ -1,11 +1,16 @@
 package ch.usi.pf2;
 
+import ch.usi.pf2.gui.LambdaFrame;
 import ch.usi.pf2.model.Interpreter;
 
+import java.awt.EventQueue;
 import java.util.Scanner;
 
 
-public class Main {
+public final class Main {
+    
+    private Main() {
+    }
     
     /**
      * Starts the untyped-lambda-calculus-interpreter application.
@@ -13,16 +18,29 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(final String[] args) {
-        final Scanner input = new Scanner(System.in);
-        boolean f = true;
-        while (f) {
-            final String a = input.nextLine();
-            if ("end".equals(a)) {
-                f = false;
-            } else {
-                System.out.println(new Interpreter().interpret(a));
-            }
+        if (args.length == 1 && args[0].equals("--gui")) {
+            runGUI();
+        } else {
+            runTUI();
         }
+    }
+    
+    private static void runTUI() {
+        final Scanner input = new Scanner(System.in);
+        final Interpreter interpreter = new Interpreter();
+        String line = input.nextLine();
+        while (!"end".equals(line)) {
+            System.out.println(interpreter.interpret(line));
+            line = input.nextLine();
+        }
+    }
+    
+    private static void runGUI() {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LambdaFrame().setVisible(true);
+            }
+        });
     }
     
 }
