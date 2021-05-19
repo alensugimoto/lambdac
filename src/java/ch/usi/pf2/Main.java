c1277ee016991e1213ecdec88abbe34c47e37f4b
Alen Sugimoto <alensugimoto@gmail.com>
2021-05-19 22:52:10 +0200

    Fix checkstyle in Main

--- src/java/ch/usi/pf2/Main.java
+++ src/java/ch/usi/pf2/Main.java
@@ -1,13 +1,17 @@
 package ch.usi.pf2;
 
-import java.util.Scanner;
 import ch.usi.pf2.model.Interpreter;
+
+import java.util.Scanner;
+
 
 public class Main {
     
-    private Main() {
-    }
-    
+    /**
+     * Starts the untyped-lambda-calculus-interpreter application.
+     * 
+     * @param args the command line arguments
+     */
     public static void main(final String[] args) {
         final Scanner input = new Scanner(System.in);
         boolean f = true;
