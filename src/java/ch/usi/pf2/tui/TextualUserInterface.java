d6f1b42c9be3ac3fd2756e9d0bc0dbba241b7017
Alen Sugimoto <alensugimoto@gmail.com>
2021-05-21 22:38:21 +0200

    Add help to TUI

--- src/java/ch/usi/pf2/tui/TextualUserInterface.java
+++ src/java/ch/usi/pf2/tui/TextualUserInterface.java
@@ -19,19 +19,32 @@ public class TextualUserInterface {
         interpreter = new Interpreter();
     }
 
+    /**
+     * Run the textual user interface.
+     */
     public void run() {
         printWelcome();
         final Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        while (!"quit".equals(input)) {
-            System.out.println(interpreter.interpret(input));
-            input = scanner.nextLine();
+        while (true) {
+            final String input = scanner.nextLine();
+            if ("exit".equals(input)) {
+                break;
+            } else if ("help".equals(input)) {
+                printHelp();
+            } else {
+                System.out.println(interpreter.interpret(input));
+            }
         }
         scanner.close();
     }
     
     private void printWelcome() {
-        System.out.println("Welcome");
+        System.out.println("Lambdac");
+        System.out.println("Type \"help\" for more information.");
+    }
+    
+    private void printHelp() {
+        System.out.println("Type \"exit\" to exit.");
     }
     
 }
